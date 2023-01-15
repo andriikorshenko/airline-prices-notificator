be21b31b42e1f70d0f55f90dd23fc1bc8f998b9b
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-15 14:02:02 +0100

    Added ConnectionString + Service

--- AirlinePricesNotificator.Services.AirlineWeb/Program.cs
+++ AirlinePricesNotificator.Services.AirlineWeb/Program.cs
@@ -1,13 +1,16 @@
+using AirlinePricesNotificator.Services.AirlineWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AirlineWebDbContext>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
