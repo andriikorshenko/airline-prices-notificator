d1084df2d4740df6be4d74b682fbdb27e1fde3dc
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-20 19:27:18 +0100

    Create Model for RabbitMQ MessaegBuss

---
+++ AirlinePricesNotificator.Services.AirlineWeb/Models/NotificationMessageDto.cs
@@ -0,0 +1,20 @@
+﻿namespace AirlinePricesNotificator.Services.AirlineWeb.Models
+{
+    public record NotificationMessageDto
+    {
+        public NotificationMessageDto()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; init; }
+
+        public string WebhookType { get; init; } = string.Empty;
+
+        public string FlightCode { get; init; } = string.Empty;
+
+        public decimal OldPrice { get; init; }
+
+        public decimal NewPrice { get; init; }
+    }
+}
