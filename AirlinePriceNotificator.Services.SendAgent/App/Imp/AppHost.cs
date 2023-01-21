c242d52e703d9ca1948ae33b292ac145276fa061
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-21 20:11:51 +0100

    Add RabbitMQ Event Listener

--- AirlinePriceNotificator.Services.SendAgent/App/Imp/AppHost.cs
+++ AirlinePriceNotificator.Services.SendAgent/App/Imp/AppHost.cs
@@ -1,10 +1,46 @@
-﻿namespace AirlinePriceNotificator.Services.SendAgent.App.Imp
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AirlinePriceNotificator.Services.SendAgent.App.Imp
 {
     public class AppHost : IAppHost
     {
         public void Run()
         {
-            Console.WriteLine("test");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "user",
+                Password = "password"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(
+                        queue: queueName,
+                        exchange: "trigger",
+                        routingKey: "");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    Console.WriteLine("Listenning on the Message Bus...");
+
+                    consumer.Received += async (ModuleHandle, ea) =>
+                    {
+                        Console.WriteLine("Event is triggered!");
+                    };
+
+                    channel.BasicConsume(
+                        queue: queueName, 
+                        autoAck: true, 
+                        consumer: consumer);
+                }
+            }
         }
     }
 }
