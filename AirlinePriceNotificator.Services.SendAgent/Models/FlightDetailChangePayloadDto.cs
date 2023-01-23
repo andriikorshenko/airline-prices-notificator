ae48745fbb8ccb4214003ff62c979a8b0188d070
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-01-23 21:54:22 +0100

    Add DTOs to SendAgent

---
+++ AirlinePriceNotificator.Services.SendAgent/Models/FlightDetailChangePayloadDto.cs
@@ -0,0 +1,19 @@
+﻿namespace AirlinePricesNotificator.Services.SendAgent.Models
+{
+    public record FlightDetailChangePayloadDto
+    {
+        public string WebhookUri { get; set; } = string.Empty;
+
+        public string Publisher { get; init; } = string.Empty;
+
+        public string Secret { get; init; } = string.Empty;
+
+        public string FlightCode { get; init; } = string.Empty;
+
+        public decimal OldPrice { get; init; }
+
+        public decimal NewPrice { get; init; }
+
+        public string WebhookType { get; set; } = string.Empty;
+    }
+}
---
+++ AirlinePriceNotificator.Services.SendAgent/Models/NotificationMessageDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AirlinePricesNotificator.Services.SendAgent.Models
+{
+    public record NotificationMessageDto
+    {
+        public string Id { get; } = string.Empty;
+
+        public string WebhookType { get; init; } = string.Empty;
+
+        public string FlightCode { get; init; } = string.Empty;
+
+        public decimal OldPrice { get; init; }
+
+        public decimal NewPrice { get; init; }
+    }
+}
